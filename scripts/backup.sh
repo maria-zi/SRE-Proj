821981caa0d0d95686dcd91bf2cb6163e1b792a9
maria-zi <102587427+maria-zi@users.noreply.github.com>
2026-06-02 15:16:46 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash  
 set -e
+# скрипт создания бэков
+BACKUP_DIR="/var/backups/telecom"  # Директория для бэкапов
+TIMESTAMP=$(date +%F-%H%M%S)   # время для имени бэка 
 
-BACKUP_DIR="/var/backups/telecom"
-TIMESTAMP=$(date +%F-%H%M%S)
-
-mkdir -p $BACKUP_DIR
+mkdir -p $BACKUP_DIR # Создаём директорию если нет
 
 echo "Creating PostgreSQL backup..."
-docker exec telecom-system-db-1 pg_dump -U telecom telecom | gzip > $BACKUP_DIR/db-$TIMESTAMP.sql.gz
-
-find $BACKUP_DIR -name "*.gz" -mtime +7 -delete
+docker exec telecom-system-db-1 pg_dump -U telecom telecom | gzip > $BACKUP_DIR/db-$TIMESTAMP.sql.gz # Делаем pg_dump (экспорт БД) и сжимаем gzip
+                                                                                                    # docker exec = выполнить команду в контейнере
+find $BACKUP_DIR -name "*.gz" -mtime +7 -delete # Удаляем бэкапы старше 7 дней (find -mtime +7 = более 7 дней)
 
 echo "Backup completed: $BACKUP_DIR/db-$TIMESTAMP.sql.gz"
