b6ea62d029ad9f50b01ceffeded08536a857b13c
maria-zi <102587427+maria-zi@users.noreply.github.com>
2026-06-02 15:13:48 +0300

    Update helm-setup.sh

--- scripts/helm-setup.sh
+++ scripts/helm-setup.sh
@@ -3,30 +3,35 @@ set -e
 
 echo "Deploying Telecom System with Helm..."
 
-if ! command -v helm &> /dev/null; then
+if ! command -v helm &> /dev/null; then  # Проверяем helm
     echo "Helm not found. Installing Helm..."
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash  # Скрипт установки Helm (официальный)
 fi
 
-if ! command -v kubectl &> /dev/null; then
+if ! command -v kubectl &> /dev/null; then  # Проверяем kubectl
     echo "kubectl not found. Install minikube/k3s first."
     exit 1
 fi
 
-# Добавляем Helm repo
+# Добавляем Helm repo (Bitnami — популярный репозиторий чартов)
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-# Создаём namespace
-kubectl create namespace telecom --dry-run=client -o yaml | kubectl apply -f -
+
+if ! kubectl get namespace telecom &> /dev/null; then  #проверка сущест ли неймспейс если нет создает
+    echo "Creating namespace 'telecom'..."
+    kubectl create namespace telecom
+else
+    echo "Namespace 'telecom' already exists"
+fi
 
 # Деплоим через Helm
 echo "Installing Helm chart..."
-helm upgrade --install telecom ./helm/telecom-chart \
-  --namespace telecom \
-  --values ./helm/telecom-chart/values.yaml
+helm upgrade --install telecom ./helm/telecom-chart \  # upgrade --install = если существует → обновить, если нет → установить
+  --namespace telecom \  
+  --values ./helm/telecom-chart/values.yaml   # --values = файл значений (переопределяет defaults в чарте)
 
-# Ждём запуска
+# Ждём запускподов
 echo "Waiting for pods to be ready..."
 kubectl wait --for=condition=ready pod --selector=app=telecom-api -n telecom --timeout=120s
 
